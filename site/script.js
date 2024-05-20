6aa761e95bbaf4a35c5e99ae72f5563ff8f040b7
ibrahim-ashraf <ibrahimashrafabdo@gmail.com>
2024-05-20 21:03:33 +0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -90,7 +90,8 @@ function getRandomSurahNumber(fromSurah, toSurah) {
 }
 
 function getRandomAyahNumber(randomSurahNumber, fromAyah1 = 1, toAyah1 = null, fromAyah2 = 1, toAyah2 = null) {
-  const surahAyahRange = surahsData[`${randomSurahNumber}`];
+  // const surahAyahRange = surahsData[`${randomSurahNumber}`];
+  alert(typeof surahsData);
   alert(surahAyahRange);
 
   // إذا لم يتم ملء حقول تحديد النطاق، اعتبر النطاق من 1 إلى عدد آيات السورة
